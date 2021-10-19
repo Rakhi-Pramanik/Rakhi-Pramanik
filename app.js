c9a460f9969d2d9d774828ceae43039272958105
Rakhi-Pramanik <rakhi.pramanik.1993@gmail.com>
2021-10-19 18:01:56 +0530

    api's changes

--- app.js
+++ app.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 
 app.get('/api/list', function (req, res) {
    post = new Post();
-   
+    
     MongoClient.connect(url, function(err, db) {
         if (err) throw err;
         var dbo = db.db(dbName);
